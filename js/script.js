53ba587cf6437d7a0a4e7e9c455c897e72a22d40
Anthony64M <anthony.mutuku@students.moringaschool.com>
2021-07-25 10:52:30 +0300

    the script for mail added

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       $("#dev-img").slideDown('1500');
     });
   });
-  
+
   $(document).ready(function(){
     $("#des-img").click(function(){
       $("#des-img").slideDown('1500').hide('1000');
@@ -19,7 +19,7 @@ $(document).ready(function(){
       $("#des-img").slideDown('1500');
     });
   });
-  
+
   $(document).ready(function(){
     $("#prod-img").click(function(){
       $("#prod-img").slideDown('1500').hide('1000');
@@ -30,72 +30,71 @@ $(document).ready(function(){
       $("#prod-img").slideDown('1500');
     });
   });
-  
-  $(document).ready(function(){
-    $("#job1").mouseover(function(){
-      $("#on-img1").show();
-    }).mouseout(function(){
-      $("#on-img1").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#job2").mouseover(function(){
-      $("#on-img2").show();
-    }).mouseout(function(){
-      $("#on-img2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#job3").mouseover(function(){
-      $("#on-img3").show();
-    }).mouseout(function(){
-      $("#on-img3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#job4").mouseover(function(){
-      $("#on-img4").show();
-    }).mouseout(function(){
-      $("#on-img4").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#job5").mouseover(function(){
-      $("#on-img5").show();
-    }).mouseout(function(){
-      $("#on-img5").hide();
-    });
-    $("#job6").mouseover(function(){
-      $("#on-img6").show();
-    }).mouseout(function(){
-      $("#on-img6").hide();
-    });
-    $("#job7").mouseover(function(){
-      $("#on-img7").show();
-    }).mouseout(function(){
-      $("#on-img7").hide();
-    });
-    $("#job8").mouseover(function(){
-      $("#on-img8").show();
-    }).mouseout(function(){
-      $("#on-img8").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("form#formDe").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
 
-    });
-
+$(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
   });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("form#form34A").submit(function(event){
+    // event.preventDefault();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+  });
+
+});
